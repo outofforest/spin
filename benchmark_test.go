1df77fceb884d9cc3c61f55f025cd935f16e5a12
Wojciech Małota-Wójcik <59281144+outofforest@users.noreply.github.com>
2023-08-28 18:17:53 +0200

    Fix benchmarks (#15)

--- benchmark_test.go
+++ benchmark_test.go
@@ -11,202 +11,240 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// go test -bench=. -run=^$ -cpuprofile profile.out
+// go tool pprof -http="localhost:8000" pprofbin ./profile.out
+
 func BenchmarkCopyNative(b *testing.B) {
+	b.StopTimer()
+	b.ResetTimer()
+
 	const loops = 1000
 
-	data := make([]byte, 10*1024) // 10 KiBs of nothing
-	result := make([]byte, len(data))
+	for bi := 0; bi < b.N; bi++ {
+		data := make([]byte, 10*1024) // 10 KiBs of nothing
+		result := make([]byte, len(data))
 
-	b.ResetTimer()
-	for i := 0; i < loops; i++ {
-		// We copy twice because in ring buffer you copy to ring buffer and then from read buffer
-		copy(result, data)
-		copy(result, data)
+		b.StartTimer()
+		for i := 0; i < loops; i++ {
+			// We copy twice because in ring buffer you copy to ring buffer and then from read buffer
+			copy(result, data)
+			copy(result, data)
+		}
+		b.StopTimer()
 	}
-	b.StopTimer()
 }
 
 func BenchmarkCopyRing(b *testing.B) {
+	b.StopTimer()
+	b.ResetTimer()
+
 	const loops = 1000
 
-	data := make([]byte, 10*1024) // 10 KiBs of nothing
-	result := make([]byte, len(data))
+	for bi := 0; bi < b.N; bi++ {
+		data := make([]byte, 10*1024) // 10 KiBs of nothing
+		result := make([]byte, len(data))
 
-	ring := New()
+		ring := New()
 
-	b.ResetTimer()
-	for i := 0; i < loops; i++ {
-		_, _ = ring.Write(data)
-		_, _ = ring.Read(result)
+		b.StartTimer()
+		for i := 0; i < loops; i++ {
+			_, _ = ring.Write(data)
+			_, _ = ring.Read(result)
+		}
+		b.StopTimer()
 	}
-	b.StopTimer()
 }
 
 func BenchmarkPerByteNative(b *testing.B) {
+	b.StopTimer()
+	b.ResetTimer()
+
 	const loops = 1000
 
-	buf := bytes.NewBuffer(make([]byte, loops))
-	mu := sync.Mutex{} // mutex is here to match the fact that ring is a concurrent-safe type
+	for bi := 0; bi < b.N; bi++ {
+		buf := bytes.NewBuffer(make([]byte, loops))
+		mu := sync.Mutex{} // mutex is here to match the fact that ring is a concurrent-safe type
 
-	b.ResetTimer()
-	for i := 0; i < loops; i++ {
-		mu.Lock()
-		_ = buf.WriteByte(0x00)
-		mu.Unlock()
+		b.StartTimer()
+		for i := 0; i < loops; i++ {
+			mu.Lock()
+			_ = buf.WriteByte(0x00)
+			mu.Unlock()
 
-		mu.Lock()
-		_, _ = buf.ReadByte()
-		mu.Unlock()
+			mu.Lock()
+			_, _ = buf.ReadByte()
+			mu.Unlock()
+		}
+		b.StopTimer()
 	}
-	b.StopTimer()
 }
 
 func BenchmarkPerByteRing(b *testing.B) {
+	b.StopTimer()
+	b.ResetTimer()
+
 	const loops = 1000
 
-	ring := New()
+	for bi := 0; bi < b.N; bi++ {
+		ring := New()
 
-	b.ResetTimer()
-	for i := 0; i < loops; i++ {
-		_ = ring.WriteByte(0x00)
-		_, _ = ring.ReadByte()
+		b.StartTimer()
+		for i := 0; i < loops; i++ {
+			_ = ring.WriteByte(0x00)
+			_, _ = ring.ReadByte()
+		}
+		b.StopTimer()
 	}
-	b.StopTimer()
 }
 
 func BenchmarkIterator(b *testing.B) {
-	const loops = 1000
-	const count = 3
-
-	requireT := require.New(b)
-
-	ring := New()
-	_, err := ring.Write(make([]byte, count*loops))
-	requireT.NoError(err)
-	requireT.NoError(ring.Close())
-
-	b.ResetTimer()
-	_, _ = ring.Iterate(func(p []byte) (int, bool, error) {
-		return count, true, nil
-	})
 	b.StopTimer()
+	b.ResetTimer()
+
+	for bi := 0; bi < b.N; bi++ {
+		const loops = 1000
+		const count = 3
+
+		requireT := require.New(b)
+
+		ring := New()
+		_, err := ring.Write(make([]byte, count*loops))
+		requireT.NoError(err)
+		requireT.NoError(ring.Close())
+
+		b.StartTimer()
+		_, _ = ring.Iterate(func(p []byte) (int, bool, error) {
+			return count, true, nil
+		})
+		b.StopTimer()
+	}
 }
 
 func BenchmarkTCPNative(b *testing.B) {
+	b.StopTimer()
+	b.ResetTimer()
+
 	requireT := require.New(b)
 
-	data := make([]byte, 100*1024*1024) // 100 MiBs of nothing
-	_, err := rand.Read(data)
-	requireT.NoError(err)
+	for bi := 0; bi < b.N; bi++ {
+		data := make([]byte, 100*1024*1024) // 100 MiBs of nothing
+		_, err := rand.Read(data)
+		requireT.NoError(err)
 
-	result := make([]byte, 10*1024)
+		result := make([]byte, 10*1024)
 
-	l, err := net.Listen("tcp", "localhost:")
-	requireT.NoError(err)
+		l, err := net.Listen("tcp", "localhost:")
+		requireT.NoError(err)
 
-	defer l.Close()
+		defer l.Close()
 
-	doneCh := make(chan struct{})
-	go func() {
-		defer close(doneCh)
+		doneCh := make(chan struct{})
+		go func() {
+			defer close(doneCh)
 
-		conn, err := l.Accept()
-		if err != nil {
-			panic(err)
-		}
-
-		_ = conn.(*net.TCPConn).SetNoDelay(true)
-		_ = conn.(*net.TCPConn).SetReadBuffer(32 * 1024)
-		_ = conn.(*net.TCPConn).SetWriteBuffer(32 * 1024)
-
-		total := 0
-		for {
-			n, err := conn.Read(result)
-			total += n
-			if total == len(data) {
-				return
-			}
-
+			conn, err := l.Accept()
 			if err != nil {
 				panic(err)
 			}
-		}
-	}()
 
-	conn, err := net.Dial("tcp", l.Addr().String())
-	requireT.NoError(err)
-	requireT.NoError(conn.(*net.TCPConn).SetNoDelay(true))
-	requireT.NoError(conn.(*net.TCPConn).SetReadBuffer(128 * 1024))
-	requireT.NoError(conn.(*net.TCPConn).SetWriteBuffer(128 * 1024))
+			_ = conn.(*net.TCPConn).SetNoDelay(true)
+			_ = conn.(*net.TCPConn).SetReadBuffer(32 * 1024)
+			_ = conn.(*net.TCPConn).SetWriteBuffer(32 * 1024)
 
-	b.ResetTimer()
+			total := 0
+			for {
+				n, err := conn.Read(result)
+				total += n
+				if total == len(data) {
+					return
+				}
 
-	_, err = conn.Write(data)
-	requireT.NoError(err)
-	requireT.NoError(conn.Close())
+				if err != nil {
+					panic(err)
+				}
+			}
+		}()
 
-	<-doneCh
+		conn, err := net.Dial("tcp", l.Addr().String())
+		requireT.NoError(err)
+		requireT.NoError(conn.(*net.TCPConn).SetNoDelay(true))
+		requireT.NoError(conn.(*net.TCPConn).SetReadBuffer(128 * 1024))
+		requireT.NoError(conn.(*net.TCPConn).SetWriteBuffer(128 * 1024))
 
-	b.StopTimer()
+		b.StartTimer()
+
+		_, err = conn.Write(data)
+		requireT.NoError(err)
+		requireT.NoError(conn.Close())
+
+		<-doneCh
+
+		b.StopTimer()
+	}
 }
 
 func BenchmarkTCPRing(b *testing.B) {
-	requireT := require.New(b)
-
-	data := make([]byte, 100*1024*1024) // 100 MiBs of nothing
-	_, err := rand.Read(data)
-	requireT.NoError(err)
-
-	ring := New()
-
-	l, err := net.Listen("tcp", "localhost:")
-	requireT.NoError(err)
-
-	defer l.Close()
-
-	done2Ch := make(chan struct{})
-	go func() {
-		defer close(done2Ch)
-
-		_, _ = ring.WriteTo(io.Discard)
-	}()
-
-	done1Ch := make(chan struct{})
-	go func() {
-		defer close(done1Ch)
-
-		conn, err := l.Accept()
-		if err != nil {
-			panic(err)
-		}
-
-		_ = conn.(*net.TCPConn).SetNoDelay(true)
-		_ = conn.(*net.TCPConn).SetReadBuffer(32 * 1024)
-		_ = conn.(*net.TCPConn).SetWriteBuffer(32 * 1024)
-
-		_, err = ring.ReadFrom(conn)
-		_ = ring.Close()
-		if err != nil {
-			panic(err)
-		}
-	}()
-
-	conn, err := net.Dial("tcp", l.Addr().String())
-	requireT.NoError(err)
-	requireT.NoError(conn.(*net.TCPConn).SetNoDelay(true))
-	requireT.NoError(conn.(*net.TCPConn).SetReadBuffer(128 * 1024))
-	requireT.NoError(conn.(*net.TCPConn).SetWriteBuffer(128 * 1024))
-
+	b.StopTimer()
 	b.ResetTimer()
 
-	_, err = conn.Write(data)
+	requireT := require.New(b)
 
-	requireT.NoError(err)
-	requireT.NoError(conn.Close())
+	for bi := 0; bi < b.N; bi++ {
+		data := make([]byte, 100*1024*1024) // 100 MiBs of nothing
+		_, err := rand.Read(data)
+		requireT.NoError(err)
 
-	<-done1Ch
+		ring := New()
 
-	b.StopTimer()
+		l, err := net.Listen("tcp", "localhost:")
+		requireT.NoError(err)
 
-	<-done2Ch
+		defer l.Close()
+
+		done2Ch := make(chan struct{})
+		go func() {
+			defer close(done2Ch)
+
+			_, _ = ring.WriteTo(io.Discard)
+		}()
+
+		done1Ch := make(chan struct{})
+		go func() {
+			defer close(done1Ch)
+
+			conn, err := l.Accept()
+			if err != nil {
+				panic(err)
+			}
+
+			_ = conn.(*net.TCPConn).SetNoDelay(true)
+			_ = conn.(*net.TCPConn).SetReadBuffer(32 * 1024)
+			_ = conn.(*net.TCPConn).SetWriteBuffer(32 * 1024)
+
+			_, err = ring.ReadFrom(conn)
+			_ = ring.Close()
+			if err != nil {
+				panic(err)
+			}
+		}()
+
+		conn, err := net.Dial("tcp", l.Addr().String())
+		requireT.NoError(err)
+		requireT.NoError(conn.(*net.TCPConn).SetNoDelay(true))
+		requireT.NoError(conn.(*net.TCPConn).SetReadBuffer(128 * 1024))
+		requireT.NoError(conn.(*net.TCPConn).SetWriteBuffer(128 * 1024))
+
+		b.StartTimer()
+
+		_, err = conn.Write(data)
+
+		requireT.NoError(err)
+		requireT.NoError(conn.Close())
+
+		<-done1Ch
+
+		b.StopTimer()
+
+		<-done2Ch
+	}
 }
